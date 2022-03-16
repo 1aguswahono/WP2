e0822a96f03419022b21ed0bdc6c483ba43542ca
1aguswahono <101736093+1aguswahono@users.noreply.github.com>
2022-03-16 23:59:11 +0700

    Add files via upload

---
+++ Contoh01.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>contoh penggunaan HTML </title>
+</head>
+<body bgcolor="lightblue">
+<h1>web programing 1 </h1>
+halo apa kabar ? <br>
+mudah mudahan kabarnya baik mudah mudahan dalam keadaan sehat wal afiat 
+</body>
+</html>
